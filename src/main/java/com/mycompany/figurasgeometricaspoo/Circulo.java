0f5034a95bd4cae748a2856cdf923cc92276bdaf
DanielFeChG <159039001+DanielFeChG@users.noreply.github.com>
2024-05-07 00:18:11 -0500

    Update Circulo.java

--- src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
@@ -1,5 +1,22 @@
 package com.mycompany.figurasgeometricaspoo;
-
-public class Circulo {
+public class Circulo extends FiguraGeometrica {
+    private double radio;
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
     
+    @Override
+    public double obtenerArea(){
+        double areaCirculo = Math.PI*(Math.pow(radio, 2));
+        return areaCirculo;
+    
+    }
+    @Override
+    public double obtenerPerimetro(){
+        double perimetroCirculo = 2*radio*Math.PI;
+        return perimetroCirculo;
+        
+   
+    }
 }
